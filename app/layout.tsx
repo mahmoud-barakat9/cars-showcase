fdc5e5e1453d2d6d0f20b9554197210629bea85b
mahmoud <mahmoudba98a@gmail.com>
2023-12-10 17:24:23 +0300

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import { Footer, Navbar } from '@/components'
+import './globals.css'
+import type { Metadata } from 'next'
+
+
+export const metadata: Metadata = {
+  title: 'Car App',
+  description: 'Discover the best car in the world.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className='relative'>
+        <Navbar/>
+        {children}
+        <Footer/>
+        </body>
+    </html>
+  )
+}
