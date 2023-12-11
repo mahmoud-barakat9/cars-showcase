3cff455f9f8bbc95844d21dee8ec58d0bd516bb5
mahmoud <mahmoudba98a@gmail.com>
2023-12-11 15:22:24 +0300

    img

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -27,7 +27,7 @@ const Hero = () => {
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <Image src="/Porsche-911.png" alt="hero" fill className="object-contain" />
         </div>
 
         <div className="hero__image-overlay" />
